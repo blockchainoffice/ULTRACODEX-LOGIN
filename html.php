e151edc593b350261fcc9f13103f04434892ac23
Blockchain Office <78378074+blockchainoffice@users.noreply.github.com>
2021-09-19 11:28:44 +0200

    Update html.php

--- html.php
+++ html.php
@@ -4,22 +4,22 @@
 <meta charset="UTF-8">
 <title>#TITLE#</title>
 <meta name="viewport" 		content="width=device-width; initial-scale=1.0; maximum-scale=4.0;" />
-<meta name="expires" 			content="0" />
-<meta name="author" 			content="Ioannis Balasis" >
+<meta name="expires" 		content="0" />
+<meta name="author" 		content="Ioannis Balasis" >
 <meta name="description" 	content="#DESCRIPTION#">
 <link rel="stylesheet" 		href="assets/css/login.css" type="text/css" media="screen" />
-<link rel="icon" 					href="assets/img/UltraCodex-Icon.ico"/>
+<link rel="icon" 		href="assets/img/UltraCodex-Icon.ico"/>
 </head>
 <body>
 	<div id="main">
 		<header>
-			<img src="assets/img/UltraCodex-Black.png" id="logo">
-			<form method="post" action="?q=1" autocomplete="new-password">
-		    <input name="#IDSEC#" type="password" maxlength="100" class="lgfld bradius" placeholder="ID" value="" autocomplete="new-password" required><br>
-		    <input name="#PWSEC#" type="password" maxlength="100" class="lgfld bradius" placeholder="PASSWORT" value="" autocomplete="new-password" required ><br>
-				<input name="#DTSEC#" type="hidden" size="23" maxlength="100" class="bradius" autocomplete="off"><br>
-				<button name="s" type="submit" value="1" data-value="#SUBSEC#" class="bradius" id="lgbtn">ANMELDEN</button>
-			</form>
+		    <img src="assets/img/UltraCodex-Black.png" id="logo">
+		    <form method="post" action="?q=1" autocomplete="new-password">
+		    	<input name="#IDSEC#" type="password" maxlength="100" class="lgfld bradius" placeholder="ID" value="" autocomplete="new-password" required><br>
+		    	<input name="#PWSEC#" type="password" maxlength="100" class="lgfld bradius" placeholder="PASSWORT" value="" autocomplete="new-password" required ><br>
+			<input name="#DTSEC#" type="hidden" size="23" maxlength="100" class="bradius" autocomplete="off"><br>
+			<button name="s" type="submit" value="1" data-value="#SUBSEC#" class="bradius" id="lgbtn">ANMELDEN</button>
+		    </form>
 		</header>
 	</div>
 </body>
